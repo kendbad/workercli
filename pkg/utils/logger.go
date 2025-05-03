81d84be08b347e5196646004c78cf092c8ee7083
phucmmo <kendbad@gmail.com>
2025-05-04 01:07:55 +0700

    update tạo file log theo định dạng ngày, cùng ngày sẽ cùng file log

--- pkg/utils/logger.go
+++ pkg/utils/logger.go
@@ -1,9 +1,10 @@
 package utils
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v3"
@@ -81,37 +82,41 @@ func loadConfig(configPath string) (*Config, error) {
 }
 
 // createLogFile tạo tệp log trong thư mục logs với tên tệp dựa trên tên thư mục hiện tại
+
 func CreateLogFile() (*os.File, error) {
-	// Lấy thư mục hiện tại
-	currentDir, err := os.Getwd()
+	// Lấy đường dẫn file thực thi
+	execPath, err := os.Executable()
 	if err != nil {
-		return nil, fmt.Errorf("Lỗi khi lấy thư mục hiện tại: %w", err)
+		return nil, err
 	}
 
-	// Lấy tên thư mục cuối cùng trong đường dẫn hiện tại
-	dirName := filepath.Base(currentDir)
+	// Lấy thư mục chứa file thực thi (cmd/workercli)
+	execDir := filepath.Dir(execPath)
 
-	// Đường dẫn đến thư mục logs
-	logDir := "logs"
+	// Giả định project root là cha của cmd/workercli
+	projectRoot := filepath.Dir(filepath.Dir(execDir))
 
-	// Tạo tên tệp log dựa trên tên thư mục hiện tại
-	logFileName := fmt.Sprintf("%s_app.log", dirName)
+	// Đường dẫn thư mục logs nằm cùng cấp với cmd/
+	logsDir := filepath.Join(projectRoot, "logs")
 
-	// Kiểm tra xem thư mục logs đã tồn tại chưa, nếu chưa thì tạo
-	if _, err := os.Stat(logDir); os.IsNotExist(err) {
-		err := os.Mkdir(logDir, os.ModePerm) // Tạo thư mục logs nếu chưa tồn tại
-		if err != nil {
-			return nil, fmt.Errorf("Không thể tạo thư mục logs: %w", err)
-		}
+	// Tạo thư mục nếu chưa tồn tại
+	if err := os.MkdirAll(logsDir, os.ModePerm); err != nil {
+		return nil, err
 	}
 
-	// Tạo đường dẫn đầy đủ đến tệp log trong thư mục logs
-	logFilePath := filepath.Join(logDir, logFileName)
+	// Tạo tên file log theo timestamp
 
-	// Tạo tệp log
-	logFile, err := os.Create(logFilePath)
+	// timestamp := time.Now().Format("20060102_150405")
+	timestamp := time.Now().Format("20060102") // chỉ lấy ngày
+
+	logFileName := filepath.Join(logsDir, "log_"+timestamp+".log")
+
+	log.Printf("Log file path: %s\n", logFileName)
+
+	// Tạo file
+	logFile, err := os.Create(logFileName)
 	if err != nil {
-		return nil, fmt.Errorf("Lỗi khi tạo file log: %w", err)
+		return nil, err
 	}
 
 	return logFile, nil
